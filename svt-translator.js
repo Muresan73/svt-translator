8dd33b2892f762511ec2183da7e1ac62b797a76b
Grge <grge73@outlook.com>
2022-10-27 22:25:22 +0200

    stub text added to the subtitle

--- svt-translator.js
+++ svt-translator.js
@@ -1,4 +1,4 @@
-document.body.style.border = 'blue 5px solid'
+document.body.style.border = 'blue 5px solid';
 
 let activatorButton = document.createElement('button');
 activatorButton.appendChild(document.createTextNode('Activate Translation'));
@@ -6,31 +6,29 @@ activatorButton.style.position = 'fixed';
 activatorButton.style.right = '0px';
 activatorButton.style.top = '0px';
 activatorButton.style.zIndex = '1000';
-activatorButton.id='activator-button'
+activatorButton.id = 'activator-button';
 document.body.appendChild(activatorButton);
 
-
 activatorButton.onclick = () => {
-  console.log("translation activated")
-  
-  console.log("subtitle_container searching...")
+  console.log('translation activated');
+
+  console.log('subtitle_container searching...');
   let subtitle_container = document.querySelector("div[class^='_video-player__text']");
-  if (subtitle_container == null) return console.log("subtitle_container can not be found")
-  console.log("subtitle_container detected")
-  
+  if (subtitle_container == null) return console.log('subtitle_container can not be found');
+  console.log('subtitle_container detected');
+
   subtitle_container.addEventListener('DOMSubtreeModified', subtitleChanged);
-  console.log("subtitle_container onchange listener attached")
-  document.body.style.border = 'red 5px solid'
-  
+  console.log('subtitle_container onchange listener attached');
+  document.body.style.border = 'red 5px solid';
+
   const translation = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve('foo');
     }, 1);
   });
 
-  console.log("stub promise initiated")
+  console.log('stub promise initiated');
 
-  
   function subtitleChanged(e) {
     console.log('subtitle changed');
 
@@ -38,13 +36,18 @@ activatorButton.onclick = () => {
 
     let text_container = subtitle_container.querySelector('p');
     let clone = text_container.firstChild.cloneNode(true);
+    text_container.appendChild(document.createElement('br'));
+    text_container.appendChild(clone);
 
     Array.from(clone.querySelectorAll('span>span>span'))
       .filter(span => !span.innerHTML.startsWith('<span>'))
-      .forEach(span => translation.then(text => (span.textContent = text)));
+      .forEach(span => span.textContent = "text");
 
-    console.log('new element added ')
-    text_container.appendChild(clone);
+    // Array.from(clone.querySelectorAll('span>span>span'))
+    //   .filter(span => !span.innerHTML.startsWith('<span>'))
+    //   .forEach(span => translation.then(text => (span.textContent = text)));
+
+    console.log('new element added ');
   }
 
   document.querySelector('#activator-button').remove();
